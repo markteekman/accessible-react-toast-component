0ff2b26a26199a9b7b0282fb8af1514d581c8d94
Mark Teekman <markteekman@users.noreply.github.com>
2024-04-24 13:56:49 +0200

    Exercise 3 addition

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -7,7 +7,10 @@ function ToastShelf({ toasts, handleDismiss }) {
   return (
     <ol className={styles.wrapper}>
       {toasts.map((toast) => (
-        <li key={toast.id}>
+        <li
+          key={toast.id}
+          className={styles.toastWrapper}
+        >
           <Toast
             id={toast.id}
             variant={toast.variant}
