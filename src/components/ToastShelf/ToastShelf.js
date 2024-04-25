363f8c7ce41ae4cf1c222e5409f9928811f9f921
Mark Teekman <markteekman@users.noreply.github.com>
2024-04-25 15:55:30 +0200

    Exercise 5

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -8,7 +8,12 @@ function ToastShelf() {
   const { toasts } = React.useContext(ToastContext);
 
   return (
-    <ol className={styles.wrapper}>
+    <ol
+      className={styles.wrapper}
+      role="region"
+      aria-live="polite"
+      aria-label="Notifications"
+    >
       {toasts.map((toast) => (
         <li
           key={toast.id}
